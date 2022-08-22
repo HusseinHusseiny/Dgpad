d4e016eaf4c8c5e140f30ebc244b17665db0b5f7
HusseinHusseiny <85897701+HusseinHusseiny@users.noreply.github.com>
2022-08-22 19:16:17 +0300

    Add files via upload

---
+++ CmsShoppingCartContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CmsShoppingCart.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CmsShoppingCart.Infrastructure
+{
+    public class CmsShoppingCartContext : DbContext
+    {
+        public CmsShoppingCartContext(DbContextOptions<CmsShoppingCartContext> options)
+            :base(options)
+        {
+
+        }
+        public DbSet<Page> Pages { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+    }
+}
